59f6dfb8f53d6d0c1148a635ac915eb8d4cda523
ZiyadRw <zeadalrouasheed@gmail.com>
2024-02-21 05:05:33 +0300

    v1.5

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom';
-import "./index.css"
-import App from './App';
+// index.js
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+reportWebVitals();
